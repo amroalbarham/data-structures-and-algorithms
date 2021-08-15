5eba1d7773abe2966e56f0f8d33a9a017f55fd47
amroalbarham <amroalbarham@gmil.com>
2021-08-15 15:54:03 +0300

    finish..

--- javascript/hashtable/__tests__/index.test.js
+++ javascript/hashtable/__tests__/index.test.js
@@ -1,5 +1,6 @@
 // import { Node, LinkedList, Hashmap } from "../index";
 const Hashmap =require('../index').Hashmap;
+const repeatedWord =require('../index').repeatedWord;
 // const LinkedList =require('../index').LinkedList;
 // const Node =require('../index').Node;
 
@@ -25,7 +26,20 @@ describe('Hashtable test ...', () => {
   test('Successfully hash a key to an in-range value ', () => {
     expect(hash.hash('amro')).toBe(189);
   })
-  
+  describe('test repeated word',()=>{
+    test('test for repeated word 1',()=>{
+        let test = '';
+        expect(repeatedWord(test)).toBe('cannot find words in an empty string');
+    })
+    test('test for repeated word 2',()=>{
+        let test = "It was the best of times, it was the worst of times, it was the age of wisdom, it was the age of foolishness, it was the epoch of belief, it was the epoch of incredulity, it was the season of Light, it was the season of Darkness, it was the spring of hope, it was the winter of despair, we had everything before us, we had nothing before us, we were all going direct to Heaven, we were all going direct the other way – in short, the period was so far like the present period, that some of its noisiest authorities insisted on its being received, for good or for evil, in the superlative degree of comparison only...";
+        expect(repeatedWord(test)).toBe('it');
+    })
+    test('test for repeated word 3',()=>{
+        let test = 'some string with no repeated words';
+        expect(repeatedWord(test)).toBe('cannot find words in an empty string');
+    })
+})
   
   
   
